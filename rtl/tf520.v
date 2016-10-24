71b73d41bf7454d590df7c98fe0e7a85ba3a7cab
stephen <stephen@developer>
2016-10-24 22:54:40 +0100

    updaing stuff

--- rtl/tf520.v
+++ rtl/tf520.v
@@ -1,37 +1,28 @@
 `timescale 1ns / 1ps
+/* 
+ 
+ This file is part of the Terrible Fire Amiga Accelerator.
+ 
+ Copyright (c) 2016, Stephen J. Leary <stephen@terriblefire.com>
+ 
+ All rights reserved.
 
-/*
-Copyright (c) 2016, Stephen J. Leary
-All rights reserved.
+ This source file is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published
+ by the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This source file is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-3. All advertising materials mentioning features or use of this software
-   must display the following acknowledgement:
-   This product includes software developed by the <organization>.
-4. Neither the name of the <organization> nor the
-   names of its contributors may be used to endorse or promote products
-   derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
-EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-module sjl520(
+module tf520(
 
            input 	 CLKCPU,
            input 	 CLK7M,
@@ -97,9 +88,9 @@ wire DTRIG_SYNC;
 
 wire CPUSPACE = &FC;
 
-wire CPCS = CPUSPACE & ({A[19:16]} === {4'b0010});
-wire BKPT = CPUSPACE & ({A[19:16]} === {4'b0000});
-wire IACK = CPUSPACE & ({A[19:16]} === {4'b1111});
+wire CPCS = CPUSPACE & ({A[19:16]} == {4'b0010});
+wire BKPT = CPUSPACE & ({A[19:16]} == {4'b0000});
+wire IACK = CPUSPACE & ({A[19:16]} == {4'b1111});
 wire DTACKPRELIM = CLK7M | CLK7MB2;
 
 FDCP #(.INIT(1'b1)) 
